0cf1b379fe8161f0bac2c29374847566f00406d9
Geisa Nepomuceno <Geisa.Nepomuceno@ibm.com>
2019-05-28 04:43:58 -0300

    Add Tool model

---
+++ src/models/Tool.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ToolSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      required: true,
+    }
+  });
+
+  mongoose.model('Tool', ToolSchema);
