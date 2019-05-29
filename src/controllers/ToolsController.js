0fe0b9549be9f0c21c14652e6024e15049ab8325
Geisa Nepomuceno <Geisa.Nepomuceno@ibm.com>
2019-05-29 17:03:11 -0300

    Add error handling

--- src/controllers/ToolsController.js
+++ src/controllers/ToolsController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Tool = mongoose.model('Tool');
 
+
 module.exports = {
   async showAll(req, res) {
     let { tag } = req.query;
@@ -9,26 +10,40 @@ module.exports = {
   },
 
   async show(req, res) {
-    const tools = await Tool.findById(req.params.id);
-
-    return res.json(tools);
+    try {
+      const tools = await Tool.findById(req.params.id)
+      return res.json(tools);
+    } catch (error) {
+      return res.status(500).send({ message: 'Provided ID is invalid.'});
+    }
   },
 
   async create(req, res) {
-    const tools = await Tool.create(req.body);
+    try {
+      const tools = await Tool.create(req.body);
+      return res.json(tools);
+    } catch (error) {
+      return res.status(500).send({ message: 'Couldn\'t create the tool. Some fields are missing...'});
+    }
 
-    return res.json(tools);
   },
 
   async update(req, res) {
-    const tools = await Tool.findByIdAndUpdate(req.params.id, req.body, { new: true });
-
-    return res.json(tools);
+    try {
+      const tools = await Tool.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      return res.json(tools);
+    } catch (error) {
+      return res.status(500).send({ message: 'Couldn\'t update the tool because provided ID is invalid.'});
+    }
   },
 
   async remove(req, res) {
-    await Tool.findByIdAndRemove(req.params.id);
+    try {
+      await Tool.findByIdAndRemove(req.params.id);
+      return res.json({});
+    } catch (error) {
+      return res.status(500).send({ message: 'Couldn\'t delete the tool because provided ID is invalid.'});
+    }
 
-    return res.json({});
   }
 }
