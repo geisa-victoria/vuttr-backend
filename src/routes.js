4eb68d8a9952465decfe1aba36d0fdf6673e736f
Geisa Nepomuceno <Geisa.Nepomuceno@ibm.com>
2019-05-29 15:57:37 -0300

    edit routes names

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const routes = express.Router();
 const ToolController = require('./controllers/ToolsController');
 
-routes.get('/tools', ToolController.index);
+routes.get('/tools', ToolController.showAll);
 routes.get('/tools/:id', ToolController.show);
-routes.post('/tools', ToolController.store);
+routes.post('/tools', ToolController.create);
 routes.put('/tools/:id', ToolController.update);
-routes.delete('/tools/:id', ToolController.destroy);
+routes.delete('/tools/:id', ToolController.remove);
 
 module.exports = routes;
