91f178b2131c991cba676bca1cf3d6222e113439
Geisa Nepomuceno <Geisa.Nepomuceno@ibm.com>
2019-06-06 09:27:04 -0300

    change http status code for responses

--- src/controllers/ToolsController.js
+++ src/controllers/ToolsController.js
@@ -1,60 +1,58 @@
-const mongoose = require("mongoose");
-const Tool = mongoose.model("Tool");
+const mongoose = require('mongoose');
+
+const Tool = mongoose.model('Tool');
 
 module.exports = {
   async showAll(req, res) {
-    let { tag } = req.query;
-
-    return res.json(await Tool.find(tag ? { tags: tag } : undefined));
+    try {
+      const { tag } = req.query;
+      return res.json(await Tool.find(tag ? { tags: tag } : undefined)).status(200);
+    } catch (error) {
+      return res.status(404).send({ message: "Couldn't find any tools on the database" });
+    }
   },
 
   async show(req, res) {
     try {
       const tools = await Tool.findById(req.params.id);
-      return res.json(tools);
+      return res.status(200).json(tools);
     } catch (error) {
-      return res.status(500).send({ message: "Provided ID is invalid." });
+      return res.status(404).send({ message: "Provided ID wasn't found on database." });
     }
   },
 
   async create(req, res) {
     try {
       const tools = await Tool.create(req.body);
-      return res.json(tools);
+      return res.status(201).json(tools);
     } catch (error) {
-      return res
-        .status(500)
-        .send({
-          message: "Couldn't create the tool. Some fields are missing..."
-        });
+      return res.status(500).send({
+        message: 'Internal server error',
+      });
     }
   },
 
   async update(req, res) {
     try {
       const tools = await Tool.findByIdAndUpdate(req.params.id, req.body, {
-        new: true
+        new: true,
       });
-      return res.json(tools);
+      return res.status(200).json(tools);
     } catch (error) {
-      return res
-        .status(500)
-        .send({
-          message: "Couldn't update the tool because provided ID is invalid."
-        });
+      return res.status(404).send({
+        message: "Couldn't update the tool because provided ID wasn't found.",
+      });
     }
   },
 
   async remove(req, res) {
     try {
       await Tool.findByIdAndRemove(req.params.id);
-      return res.json({});
+      return res.status(204).json({});
     } catch (error) {
-      return res
-        .status(500)
-        .send({
-          message: "Couldn't delete the tool because provided ID is invalid."
-        });
+      return res.status(404).send({
+        message: "Couldn't delete the tool because provided ID wasn't found",
+      });
     }
-  }
+  },
 };
